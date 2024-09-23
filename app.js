5bb48a487351cb34eb16f5dda0b40861d998fef8
j04quin <119075762+j04quin@users.noreply.github.com>
2024-09-23 22:36:44 +0000

    correccion

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
+let pronoun = ["the", "our", "last"];
+let adj = ["great", "big", "of"];
+let noun = ["jogger", "racoon", "us"];
+let domain = ["com", "io", "net", "uy", "us"];
+
 let generateDomainName = () => {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let domain = ["com", "io", "net", "uy"];
-
-  function randomNumberTwoIndex() {
-    return Math.round(Math.random());
+  for (const p of pronoun) {
+    for (const a of adj) {
+      for (const n of noun) {
+        for (const d of domain) {
+          if (n.endsWith(d)) {
+            console.log(`${p}${a}${n.slice(0, n.length - d.length)}.${d}`);
+          } else {
+            console.log(`${p}${a}${n}.${d}`);
+          }
+        }
+      }
+    }
   }
-  function randomNumberMoreIndex(num) {
-    return Math.floor(Math.random() * num);
-  }
-
-  return (
-    pronoun[randomNumberTwoIndex()] +
-    adj[randomNumberTwoIndex()] +
-    noun[randomNumberTwoIndex()] +
-    domain[randomNumberMoreIndex(domain.length)]
-  );
 };
-
-console.log(generateDomainName());
+generateDomainName();
